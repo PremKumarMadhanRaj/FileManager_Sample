45f802d15d1e31d8ec49221dd0e8efb411ace665
premkumar.madhanraj <premkumar.madhanraj@syncfusion.com>
2021-12-03 10:56:32 +0530

    File manager samples added

---
+++ src/file-manager/models/navigation-pane-settings-model.d.ts
@@ -0,0 +1,36 @@
+import { Property, ChildProperty } from '@syncfusion/ej2-base';import { SortOrder } from '../base/interface';
+
+/**
+ * Interface for a class NavigationPaneSettings
+ */
+export interface NavigationPaneSettingsModel {
+
+    /**
+     * Specifies the maximum width of navigationpane.
+     *
+     * @default '650px'
+     */
+    maxWidth?: string | number;
+
+    /**
+     * Specifies the minimum width of navigationpane.
+     *
+     * @default '240px'
+     */
+    minWidth?: string | number;
+
+    /**
+     * Enables or disables the navigation pane.
+     *
+     * @default true
+     */
+    visible?: boolean;
+
+    /**
+     * Enables or disables the navigation pane.
+     *
+     * @default 'Ascending''
+     */
+    sortOrder?: SortOrder;
+
+}
